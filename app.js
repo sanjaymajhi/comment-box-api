ab44673aa8e8a8ebb7c5c33c8bb421e7d1bad1f0
Sanjay Majhi <47259000+sanjaymajhi@users.noreply.github.com>
2020-05-29 20:42:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,9 @@ app.use(function (err, req, res, next) {
 });
 
 var mongoose = require("mongoose");
-var mongoDB = process.env.MONGODB_URI;
+var dev_db_URl='mongodb+srv://sanjay:1Sanjay@@cluster0-1naxh.mongodb.net/test?retryWrites=true&w=majority';
+
+var mongoDB=process.env.MONGODB_URI || dev_db_URl;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
